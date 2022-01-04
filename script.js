1a2a35fe3dba2b865b4fd4e36e9a3d7a3564bf86
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-01-04 17:55:52 -0500

    added enter button, added popup screen, encorporated escape button

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const popup = document.querySelector(".popup");
+const overlay = document.querySelector(".overlay");
+const btnClosePopup = document.querySelector(".close-popup");
+const btnsOpenPopup = document.querySelectorAll(".show-popup");
 
+const openPopup = function () {
+  popup.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closePopup = function () {
+  popup.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenPopup.length; i++)
+  btnsOpenPopup[i].addEventListener("click", openPopup);
+
+btnClosePopup.addEventListener("click", closePopup);
+overlay.addEventListener("click", closePopup);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !popup.classList.contains("hidden")) {
+    closePopup();
+  }
+});
